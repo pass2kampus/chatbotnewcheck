96d95385ed18447b442f86e90a05d855df45331c
pass2kampus <pass2kampus@gmail.com>
2025-07-03 16:33:54 +0200

    Fix Index.tsx import path

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index.tsx";
+import Index from "../pages/Index";
 import NotFound from "./pages/NotFound";
 import { NotificationProvider } from "@/hooks/useNotifications";
 import React from "react";
