b3c58b0515eaae234d085aa7d6a65bb30c43f441
pass2kampus <pass2kampus@gmail.com>
2025-07-07 14:31:43 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "/pages/Index";
-import NotFound from "/pages/NotFound";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
 import { NotificationProvider } from "@/hooks/useNotifications";
 import React from "react";
 
